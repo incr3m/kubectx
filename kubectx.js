6d81ca879503a68386e1457e83796974e2435994
jim <thecastlefaller@gmail.com>
2019-02-17 15:32:36 +0800

    convert to promise

--- kubectx.js
+++ kubectx.js
@@ -1,24 +1,35 @@
 #!/usr/bin/env node
 
-const execa = require('execa');
-const inquirer = require('inquirer');
+const execa = require("execa");
+const { execSync } = require("child_process");
+const inquirer = require("inquirer");
 
-Promise.all([
-  execa('kubectl', 'config current-context'.split(' ')),
-  execa('kubectl', 'config get-contexts -o name'.split(' ')),
-])
-  .then(([{ stdout: currentContext }, { stdout: contexts }]) =>
-    inquirer.prompt([
+(async () => {
+  try {
+    const [
+      { stdout: currentContext },
+      { stdout: contexts }
+    ] = await Promise.all([
+      execa("kubectl", "config current-context".split(" ")),
+      execa("kubectl", "config get-contexts -o name".split(" "))
+    ]);
+
+    const { context: selectedContext } = await inquirer.prompt([
       {
-        type: 'list',
-        message: 'Select kubectl context',
-        name: 'context',
+        type: "list",
+        message: "Select kubectl context",
+        name: "context",
         default: currentContext,
-        choices: contexts.split('\n').sort(),
-      },
-    ])
-  )
-  .then(({ context: selectedContext }) =>
-    execa('kubectl', 'config use-context'.split(' ').concat(selectedContext))
-  )
-  .then(({ stdout: result }) => console.log(result));
+        choices: contexts.split("\n").sort()
+      }
+    ]);
+
+    execSync("kubectl config use-context " + selectedContext);
+
+    console.log("done!");
+    process.exit(0);
+  } catch (err) {
+    console.log("error", err); //TRACE
+    process.exit(1);
+  }
+})();
